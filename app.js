eabb7995b59fc864e291948b9df05016a4704b86
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-04 16:17:37 +0530

    Add customer table display and refactor customer addition functionality

--- app.js
+++ app.js
@@ -116,32 +116,45 @@
 
 let customerList = [];
 
-function btnAddCustomerOnAction() {
+function btnAddCustomerOnAction(){
     let txtName = document.getElementById("txtName").value;
     let txtAddress = document.getElementById("txtAddress").value;
     let txtAge = document.getElementById("txtAge").value;
     let txtEmail = document.getElementById("txtEmail").value;
     let txtSalary = document.getElementById("txtSalary").value;
 
-
-    let customer = {
-        name: txtName,
-        address: txtAddress,
-        age: txtAge,
-        email: txtEmail,
-        salary: txtSalary
-
+    let customer={
+        name:txtName,
+        address:txtAddress,
+        age:txtAge,
+        email:txtEmail,
+        salary:txtSalary
     }
-    
+
     customerList.push(customer);
+    loadTable();
+
     console.log(customerList);
-
-
+      
 }
 
+function loadTable(){
+    let tblCustomers = document.getElementById("tblCustomers");
 
+    let body ="";
 
+    for(let i =0; i<customerList.length; i++){
+            body+=`<tr>
+            <td>${customerList[i].name}</td>
+            <td>${customerList[i].address}</td>
+            <td>${customerList[i].age}</td>
+            <td>${customerList[i].email}</td>
+            <td>${customerList[i].salary}</td>
+        </tr>`;
+    }
 
+    tblCustomers.innerHTML=body;
 
-
-
+    console.log(tblCustomers);
+    
+}

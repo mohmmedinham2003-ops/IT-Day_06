cd4cf38fe1d2c203021f1230bfe25cc4049bbfec
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-04 11:13:07 +0530

    Update HTML title and add heading element; comment out customer2 object in JavaScript

--- app.js
+++ app.js
@@ -14,47 +14,47 @@
 // console.log(customer1);
 
 
-let customer2 = {
-    name: "John",
-    age: 30,
-    address: "wattala",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item2",
-            price: 200,
-            variants: [
-                {
-                    id: 1,
-                    name:"variant1",
-                    price:10
-                },
-                {
-                    id:2,
-                    name:"variant2",
-                    price:12
-                }
+// let customer2 = {
+//     name: "John",
+//     age: 30,
+//     address: "wattala",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item2",
+//             price: 200,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name:"variant1",
+//                     price:10
+//                 },
+//                 {
+//                     id:2,
+//                     name:"variant2",
+//                     price:12
+//                 }
 
-            ]
-        },
-        {
-            id: 2,
-            name: "item2",
-            price: 100,
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300,
-        }
-    ],
-    father: {
-        name: "inham",
-        age: 50,
-        address: "chicago"
+//             ]
+//         },
+//         {
+//             id: 2,
+//             name: "item2",
+//             price: 100,
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300,
+//         }
+//     ],
+//     father: {
+//         name: "inham",
+//         age: 50,
+//         address: "chicago"
 
-    }
-};
+//     }
+// };
 
 // console.log(customer2.name);
 // console.log(customer2.age);
@@ -64,7 +64,13 @@ let customer2 = {
 // console.log(customer2.father);
 // console.log(customer2.items[2]);
 // console.log(customer2.father.name);
-console.log(customer2.items[0].variants[0].name);
+// console.log(customer2.items[0].variants[0].name);
+
+// document.write("<p>vksjdvljsdvljnjlvnsjdvn kdfsdnfsdnffd ififushfiusdf</p>");
+console.log(document.title);
+
+let heading = document.getElementById("heading");
+console.log(heading);
 
 
 

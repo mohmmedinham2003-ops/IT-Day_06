fbed3ad70de776c6f899de69509d45bdb0725085
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-04 09:52:08 +0530

    Refactor customer class implementation and add customer2 object

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("HI");
+// class Customer {
+//     name;
+//     age;
+//     address;
+
+//     constructor(name, age, address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+// }
+
+// let customer1 = new Customer("Sharada", 20, "Wattala");
+// console.log(customer1);
+
+
+let customer2 = {
+    name: "John",
+    age: 30,
+    address: "wattala"
+};
+
+console.log(customer2);

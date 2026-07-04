cd56469a150a4c1163bb7dfbf354a4b72db7b9e8
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-04 10:02:51 +0530

    Add items and father properties to customer2 object

--- app.js
+++ app.js
@@ -18,11 +18,26 @@ let customer2 = {
     name: "John",
     age: 30,
     address: "wattala",
-    salary : 50000
+    salary: 50000,
+    items:["item1","item2","item3"],
+    father:{
+        name:"inham",
+        age:50,
+        address:"chicago"
+
+    }
 };
 
 console.log(customer2.name);
 console.log(customer2.age);
 console.log(customer2.address);
 console.log(customer2.salary);
+console.log(customer2.items);
+console.log(customer2.father);
+console.log(customer2.items[2]);
+console.log(customer2.father.name);
+
+
+
+
 

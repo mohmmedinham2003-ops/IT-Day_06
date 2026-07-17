a1bf0246c1439938579caefbd06f13189ed9f7fd
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-17 20:03:14 +0530

    Uncomment customer2 object and related console logs for execution

--- app.js
+++ app.js
@@ -30,63 +30,63 @@
 // console.log(customer1);
 
 
-let customer2 = {
-    name: "John",
-    age: 30,
-    address: "wattala",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item2",
-            price: 200,
-            variants: [
-                {
-                    id: 1,
-                    name:"variant1",
-                    price:10
-                },
-                {
-                    id:2,
-                    name:"variant2",
-                    price:12
-                }
+// let customer2 = {
+//     name: "John",
+//     age: 30,
+//     address: "wattala",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item2",
+//             price: 200,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name:"variant1",
+//                     price:10
+//                 },
+//                 {
+//                     id:2,
+//                     name:"variant2",
+//                     price:12
+//                 }
 
-            ]
-        },
-        {
-            id: 2,
-            name: "item2",
-            price: 100,
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300,
-        }
-    ],
-    father: {
-        name: "inham",
-        age: 50,
-        address: "chicago"
+//             ]
+//         },
+//         {
+//             id: 2,
+//             name: "item2",
+//             price: 100,
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300,
+//         }
+//     ],
+//     father: {
+//         name: "inham",
+//         age: 50,
+//         address: "chicago"
 
-    },
-    location:"Colombo"
+//     },
+//     location:"Colombo"
 
     
-};
+// };
 
 
 
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items);
-console.log(customer2.father);
-console.log(customer2.items[2]);
-console.log(customer2.father.name);
-console.log(customer2.items[0].variants[0].name);
-console.log(customer2.location);
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items);
+// console.log(customer2.father);
+// console.log(customer2.items[2]);
+// console.log(customer2.father.name);
+// console.log(customer2.items[0].variants[0].name);
+// console.log(customer2.location);
 
 let employee = {
     id: "E001",
@@ -164,6 +164,8 @@ console.log(employee.company);
 
 
 // let heading = document.getElementById("heading");
+
+let heading = document,getElementById("heading");
 
 // // heading.innerText = "lapulga";
 // let number = 0;
